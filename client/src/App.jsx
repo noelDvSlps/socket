14e4536ad55800ffb815d3a122dbe1bf07bdbe85
Noel Pulido <noelpulido1229@gmail.com>
2024-06-15 20:48:40 -0700

    better

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,13 @@ import io from "socket.io-client";
 const socket = io.connect("https://socket-e3rd.onrender.com");
 
 function App() {
+  // Get the scrollable div element
+  let scrollableDiv = document.getElementById("scrollableDiv");
+
+  // Function to scroll to the bottom of the div
+  const scrollToBottom = () => {
+    scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
+  };
   const joinRoom = () => {
     if (room !== "") {
       socket.emit("join-room", room);
@@ -11,18 +18,23 @@ function App() {
   };
 
   const sendMessage = (message) => {
-    socket.emit("send-message", { message, room });
+    const userId = socket.id;
+    socket.emit("send-message", { message, room, userId });
   };
 
   const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState("");
+  const [messageReceived, setMessageReceived] = useState([]);
   const [room, setRoom] = useState("");
 
   useEffect(() => {
     socket.on("receive-message", (data) => {
-      setMessageReceived(data.message);
+      console.log("received");
+      console.log(data);
+      // setMessageReceived(data.message);
+      setMessageReceived(data);
     });
   }, []);
+
   return (
     <div>
       <input
@@ -37,7 +49,39 @@ function App() {
         onChange={(e) => setMessage(e.target.value)}
       />
       <button onClick={() => sendMessage(message)}> Send Message</button>
-      <h2>{messageReceived}</h2>
+      {/* <h2>{messageReceived}</h2> */}
+      <div
+        id="scrollableDiv"
+        style={{
+          border: "solid",
+          height: "30vh",
+          width: "80vw",
+          overflowY: "scroll",
+          scroll,
+        }}
+      >
+        {
+          <ul style={{ listStyle: "none" }}>
+            {messageReceived.map((data, i) => {
+              // setTimeout(() => {
+              //   scrollToBottom();
+              // }, 100);
+              return (
+                <li
+                  key={i}
+                  style={{
+                    textAlign: data.userId === socket.id ? "end" : "start",
+                  }}
+                >
+                  {data.userId === socket.id ? "Me:" : data.userId + ":"}{" "}
+                  {data.message}
+                  {scrollToBottom()}
+                </li>
+              );
+            })}
+          </ul>
+        }
+      </div>
     </div>
   );
 }
