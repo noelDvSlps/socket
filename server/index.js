ddcc6fc6559ac2ef03639c6ad387ed24a1fc2f25
Noel Pulido <noelpulido1229@gmail.com>
2024-06-15 20:48:10 -0700

    better

--- server/index.js
+++ server/index.js
@@ -11,24 +11,26 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://socket-swart.vercel.app",
+    // origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
+let allData = [];
+
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
+  // console.log(`User connected: ${socket.id}`);
 
   socket.on("join-room", (data) => {
     socket.join(data);
   });
 
   socket.on("send-message", (data) => {
-    socket.to(data.room).emit("receive-message", data);
+    console.log(`User connected: ${socket.id}`);
+    allData = [...allData, data];
+    // socket.to(data.room).emit("receive-message", allData);
+    io.sockets.in(data.room).emit("receive-message", allData);
   });
-
-  //   socket.on("send-message", (data) => {
-  //     socket.broadcast.emit("receive-message", data);
-  //   });
 });
 
 server.listen(3001, () => {
